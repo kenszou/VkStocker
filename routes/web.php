64bf9e497efd56aa7198ab46183bfbe667613b3e
kenszou <kensou.give@gmail.com>
2024-04-01 15:12:28 -0300

    First commit

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('template');
+});
+
+Route::get('/login', function () {
+    return view('auth.login');
+});
+
+Route::get('/registro', function () {
+    return view('auth.registro');
+});
+
+Route::get('/404', function () {
+    return view('pages.404');
+});
+
+Route::get('/401', function () {
+    return view('pages.401');
+});
+
+Route::get('/500', function () {
+    return view('pages.500');
+});
